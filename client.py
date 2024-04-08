f5f344a50878ee656fc4d48b21600059a67182b4
Adolfo Gante <adolfogante@gmail.com>
2024-04-08 00:25:22 -0700

    initial commit

---
+++ client.py
@@ -0,0 +1,132 @@
+from socket import *
+import pickle
+
+# Transaction Class
+class Transaction:
+    def __init__(self, id, payer, transfer_amount, payee1, received_amount_payee1, status, payee2=None, received_amount_payee2=0):
+        self.id = id
+        self.payer = payer
+        self.transfer_amount = transfer_amount
+        self.payee1 = payee1
+        self.received_amount_payee1 = received_amount_payee1
+        self.status = status
+        self.payee2 = payee2
+        self.received_amount_payee2 = received_amount_payee2
+    
+    def __str__(self):
+        return (f"TX: id = '{self.id}', payer = '{self.payer}', transfer_amount = '{self.transfer_amount}', payee1 = '{self.payee1}', received_amount_payee1 = '{self.received_amount_payee1}', payee2 = '{self.payee2}', received_amount_payee2 = '{self.received_amount_payee2}', status = '{self.status}'")
+    
+    def __repr__(self):
+        return (f"TX: id = '{self.id}', payer = '{self.payer}', transfer_amount = '{self.transfer_amount}', payee1 = '{self.payee1}', received_amount_payee1 = '{self.received_amount_payee1}', payee2 = '{self.payee2}', received_amount_payee2 = '{self.received_amount_payee2}', status = '{self.status}'")
+
+transactions = []
+username = "0"
+balance = 0
+
+serverName = 'localhost'
+serverPort = 12000
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+
+#  Authentication Flag to let user access server or prompt user to try again
+isAuthenticated = "False"
+
+# Authentication Loop
+while isAuthenticated == "False":
+    
+	# Send Username to Server for Authentication
+	username_message = input("Enter Username: ")
+	username = username_message
+	clientSocket.sendto(username_message.encode(),(serverName, serverPort))
+	if (username_message == "quit"):
+		break
+
+	# Send Password to Server for Authentication
+	password_message = input("Enter Password: ")
+	clientSocket.sendto(password_message.encode(),(serverName, serverPort))
+	if (password_message == "quit"):
+		break
+
+	# Print authentication message to User
+	authentication_message, serverAddress = clientSocket.recvfrom(2048)
+	print (authentication_message.decode())
+ 
+	# Get isAuthenticated Value From Server
+	isAuthenticated_message, serverAddress = clientSocket.recvfrom(2048)
+	isAuthenticated = isAuthenticated_message.decode()
+
+# Dictionaries
+transactionID = {
+    "A": 100, 
+    "B": 200, 
+    "C": 300, 
+    "D": 400
+}
+
+payee1Options = {
+    "A": "1. B, 2. C, 3. D", 
+    "B": "1. A, 2. C, 3. D", 
+    "C": "1. A, 2. B, 3. D", 
+    "D": "1. A, 2. B, 3. C"
+}
+
+payee2Options = {
+	"AB": "1. C, 2. D",
+	"AC": "1. B, 2. D",
+	"AD": "1. B, 2. C",
+ 	"BA": "1. C, 2. D",
+	"BC": "1. A, 2. D",
+	"BD": "1. A, 2. C",
+  	"CA": "1. B, 2. D",
+	"CB": "1. A, 2. D",
+	"CD": "1. A, 2. B",
+  	"DA": "1. B, 2. C",
+	"DB": "1. A, 2. C",
+	"DC": "1. A, 2. B",
+}
+
+# Program Loop
+while isAuthenticated == "True":
+    print ("(1) Make a Transaction\n(2) Fetch and display the list of transactions\n(3) Quit the Program")
+    menu_option = input("Enter Number: ")
+    clientSocket.sendto(menu_option.encode(),(serverName, serverPort))
+    
+    if (menu_option == "1"):
+        id = 0
+        transfer_amount = int(input("How much to transfer?: "))
+        payee1 = input("Who will be Payee1? " + payee1Options[username] + ": ")
+        received_amount_payee1 = int(input("How much will Payee1 receive?: "))
+        payee2 = "0"
+        received_amount_payee2 = 0
+        while 1:
+            if (received_amount_payee1 > transfer_amount):
+                print("Transfer Amount: " + str(transfer_amount))
+                print("Enter a number equal to or less than Transfer Amount.")
+                received_amount_payee1 = int(input("How much will Payee1 receive?: "))
+            if (received_amount_payee1 == transfer_amount):
+                print ("Payee1 will receive: " + str(transfer_amount))
+                break
+            if (received_amount_payee1 < transfer_amount):
+                payee2 = input("Who will be Payee2? " + payee2Options[username + payee1] + ": ")
+                received_amount_payee2 = transfer_amount - received_amount_payee1
+                print ("Payee2 will receive: " + str(received_amount_payee2))
+                break  
+        status = 1
+        if (len(transactions) == 0):
+            id = transactionID[username]
+        else:
+            id = transactions[len(transactions) - 1] + 1
+        
+        temp_transaction = Transaction(id, username, transfer_amount, payee1, received_amount_payee1, status, payee2, received_amount_payee2)
+        transactions.append(temp_transaction)
+    
+    if (menu_option == "2"):
+        if (len(transactions) == 0):
+            print ("No transactions in list.")
+        for tx in transactions:
+            print(tx)
+        
+    if (menu_option == "3"):
+        print ("Quiting Program")
+        break
+
+clientSocket.close()
---
+++ server.py
@@ -0,0 +1,112 @@
+from socket import *
+
+# Server User Class
+class User:
+    def __init__(self, username, password, balance, transactions):
+        self.username = username
+        self.password = password
+        self.balance = balance
+        self.transactions = transactions
+        
+class Transaction:
+    def __init__(self, id, payer, transfer_amount, payee1, received_amount_payee1, status, payee2=None, received_amount_payee2=0):
+        self.id = id
+        self.payer = payer
+        self.transfer_amount = transfer_amount
+        self.payee1 = payee1
+        self.received_amount_payee1 = received_amount_payee1
+        self.status = status
+        self.payee2 = payee2
+        self.received_amount_payee2 = received_amount_payee2
+    
+    def __str__(self):
+        return (f"TX: id = '{self.id}', payer = '{self.payer}', transfer_amount = '{self.transfer_amount}', payee1 = '{self.payee1}', received_amount_payee1 = '{self.received_amount_payee1}', payee2 = '{self.payee2}', received_amount_payee2 = '{self.received_amount_payee2}', status = '{self.status}'")
+    
+    def __repr__(self):
+        return (f"TX: id = '{self.id}', payer = '{self.payer}', transfer_amount = '{self.transfer_amount}', payee1 = '{self.payee1}', received_amount_payee1 = '{self.received_amount_payee1}', payee2 = '{self.payee2}', received_amount_payee2 = '{self.received_amount_payee2}', status = '{self.status}'")
+    
+def authenticate(username, password, users):
+    for index, user in enumerate(users):
+        if user.username == username and user.password == password:
+            return True, users[index]
+    return False, User("0","0", 0, [])
+
+def storeTransactions(transactions, user):
+    comfirmed_transactions = []
+    for index, users in enumerate(transactions):
+        if (transactions[index].payer == user and transactions[index].status == 2):
+            comfirmed_transactions.append(transactions[index]) 
+
+# Users
+UserA = User("A", "A", 10, [])
+UserB = User("B", "B", 10, [])
+UserC = User("C", "C", 10, [])
+UserD = User("D", "D", 10, [])
+
+# Users List
+users = [UserA, UserB, UserC, UserD]
+
+# Comfirmed Transactions (TX) List
+transactions = []
+
+# UDP Server Setup
+serverPort = 12000
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+serverSocket.bind(('', serverPort))
+print ('The server is ready to receive')
+
+# Program Loop
+
+while 1:
+    # Authentication flag to either let user access server or login to server
+    isAuthenticated = False
+    
+    # Most Recent User on Server
+    currentUser = User("0","0", 0, [])
+    
+    # Authentication Loop
+    while not isAuthenticated:
+        # Recive username as message form client user
+        username_message, clientAddress = serverSocket.recvfrom(2048)
+        
+        if (username_message.decode() == "quit"): 
+            print ("User has quit the client program.")
+            break      
+         
+        # Recive password as message form client user
+        password_message, clientAddress = serverSocket.recvfrom(2048)
+        print ("Recived an Authentication Request from User " + username_message.decode())
+        
+        if (password_message.decode() == "quit"): 
+            print ("User has quit the client program.")
+            break
+        
+        # Authenticate User
+        isAuthenticated, currentUser = authenticate(username_message.decode(), password_message.decode(), users)
+        
+        # If Authentication Failed
+        if not isAuthenticated:
+            serverSocket.sendto("Authentication Failed. :(\nEnter Username and Password again.\nOr Quit Program (Enter 'quit').".encode(), clientAddress)
+            serverSocket.sendto("False".encode(), clientAddress)
+            print ("Failed to Authenticate User " + username_message.decode())
+        
+        # If Authentication was successful
+        if isAuthenticated:
+            serverSocket.sendto(("Authentication Successful!\nCurrent Balance: " + str(currentUser.balance) + " BTC").encode(), clientAddress)
+            serverSocket.sendto("True".encode(), clientAddress)
+            print ("User " + username_message.decode() + " is Authenticated!")
+            break
+    
+    # Authenticated User Options Loop
+    while isAuthenticated:
+        options_message, clientAddress = serverSocket.recvfrom(2048)
+        option = options_message.decode()
+        
+        if (option == "1"):
+            print()
+        if (option == "2"):
+            print()
+        if (option == "3"):
+            print ("User " + currentUser.username + " has logged out.")
+            break;
+            
